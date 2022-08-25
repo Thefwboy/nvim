2ec7b6dbc58cc85c108589e0ba0485e7e9c3b339
thefwboy <xk18830084945@gmail.com>
2022-08-25 22:37:46 +0800

    base config

---
+++ after/plugin/keybind.lua
@@ -0,0 +1,61 @@
+local opt = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local map = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+map("", "<Space>", "<Nop>", opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- 取消 s 默认功能
+map("n", "s",     "",       opt)
+-- windows 分屏快捷键
+map("n", "sv", ":vsp<CR>", opt)
+map("n", "sh", ":sp<CR>", opt)
+-- 关闭当前
+map("n", "sc", "<C-w>c", opt)
+-- 关闭其他
+map("n", "so", "<C-w>o", opt)
+-- Alt + hjkl  窗口之间跳转
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+-- 左右比例控制
+map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
+map("n", "s,", ":vertical resize -20<CR>", opt)
+map("n", "s.", ":vertical resize +20<CR>", opt)
+-- 上下比例
+map("n", "sj", ":resize +10<CR>", opt)
+map("n", "sk", ":resize -10<CR>", opt)
+map("n", "<C-Down>", ":resize +2<CR>", opt)
+map("n", "<C-Up>", ":resize -2<CR>", opt)
+-- 等比例
+map("n", "s=", "<C-w>=", opt)
+
+-- visual模式下缩进代码
+map("v", "<", "<gv", opt)
+map("v", ">", ">gv", opt)
+
+-- 上下移动选中文本
+map("v", "J", ":move '>+1<CR>gv-gv", opt)
+map("v", "K", ":move '<-2<CR>gv-gv", opt)
+
+-- 上下滚动浏览
+map("n", "<C-j>", "4j", opt)
+map("n", "<C-k>", "4k", opt)
+-- ctrl u / ctrl + d  只移动9行，默认移动半屏
+map("n", "<C-u>", "9k", opt)
+map("n", "<C-d>", "9j", opt)
---
+++ after/plugin/options.lua
@@ -0,0 +1,66 @@
+local options = {
+  -- utf8
+  encoding = "UTF-8",
+  fileencoding = "utf-8",
+  -- 使用相对行号
+  number = true,
+  relativenumber = true,
+  -- 缩进2个空格等于一个Tab
+  tabstop = 2,
+  softtabstop = 2,
+  shiftround = true,
+  -- >> << 时移动长度
+  shiftwidth = 2,
+  -- 空格替代tab
+  expandtab = true,
+  -- 新行对齐当前行
+  autoindent = true,
+  smartindent = true,
+  -- 搜索大小写不敏感，除非包含大写
+  ignorecase = true,
+  smartcase = true,
+  -- 高亮所在行
+  cursorline = true,
+  -- 显示左侧图标指示列
+  signcolumn = "yes",
+  -- jkhl 移动时光标周围保留8行
+  scrolloff = 8,
+  sidescrolloff = 8,
+  -- 边输入边搜索
+  incsearch = true,
+  -- 当文件被外部程序修改时，自动加载
+  autoread = true,
+  -- 禁止创建备份文件
+  backup = false,
+  writebackup = false,
+  swapfile = false,
+  -- smaller updatetime
+  updatetime = 300,
+  -- 设置 timeoutlen 为等待键盘快捷键连击时间500毫秒
+  timeoutlen = 500,
+  -- split window 从下边和右边出现
+  splitbelow = true,
+  splitright = true,
+  -- 自动补全不自动选中
+  completeopt = "menu,menuone,noselect,noinsert",
+  -- 样式
+  background = "dark",
+  termguicolors = true,
+  -- 不可见字符的显示，这里只把空格显示为一个点
+  -- list = true,
+  -- listchars = "space:·",
+  -- 补全增强
+  wildmenu = true,
+  -- Dont' pass messages to |ins-completin menu|
+  shortmess = vim.o.shortmess .. 'c',
+  -- 补全最多显示10行
+  pumheight = 10,
+  -- 永远显示 tabline
+  showtabline = 2,
+  -- 使用增强状态栏插件后不再需要 vim 的模式提示
+  showmode = false
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
