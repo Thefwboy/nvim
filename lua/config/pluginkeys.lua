e6e282e970945fac2a5e1419c9d02ca508e15cde
thefwboy <xk18830084945@gmail.com>
2022-08-27 10:31:22 +0800

    nvimtree

---
+++ lua/config/pluginkeys.lua
@@ -0,0 +1,38 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+-- 插件快捷键
+local pluginKeys = {}
+
+-- nvim-tree
+-- alt + m 键打开关闭tree
+keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+-- 列表快捷键
+pluginKeys.NvimTreeList = {
+  -- 打开文件或文件夹
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
+  -- 分屏打开文件
+  { key = "v", action = "vsplit" },
+  { key = "h", action = "split" },
+  -- 显示隐藏文件
+  { key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
+  { key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
+  -- 文件操作
+  { key = "<F5>", action = "refresh" },
+  { key = "a", action = "create" },
+  { key = "d", action = "remove" },
+  { key = "r", action = "rename" },
+  { key = "x", action = "cut" },
+  { key = "c", action = "copy" },
+  { key = "p", action = "paste" },
+  { key = "s", action = "system_open" },
+}
+return pluginKeys
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,6 +43,14 @@ return packer.startup(function(use)
     end
   }
 
+  use {
+    "kyazdani42/nvim-tree.lua",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("config.nvim-tree").setup()
+    end
+  }
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
